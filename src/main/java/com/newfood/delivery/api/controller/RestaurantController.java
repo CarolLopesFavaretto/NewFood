15b2c68a1c33375ebec9ae3510d7dd1718084f71
CarolLopesFavaretto <carolinelopesrdg@hotmail.com>
2022-08-19 17:57:16 -0300

    Implementação criteria query com predicates;

--- src/main/java/com/newfood/delivery/api/controller/RestaurantController.java
+++ src/main/java/com/newfood/delivery/api/controller/RestaurantController.java
@@ -14,6 +14,7 @@ import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -36,6 +37,11 @@ public class RestaurantController {
     @GetMapping("/for-cuisine")
     public List<Restaurant> getByName(@RequestParam String name, Long cuisineId) {
         return repository.getByNameAndCuisine(name,cuisineId);
+    }
+
+    @GetMapping("/for-shipping")
+    public List<Restaurant> getByShipping (@RequestParam String name, BigDecimal shippingBegin, BigDecimal shippingFinal) {
+        return repository.find(name,shippingBegin, shippingFinal);
     }
     @GetMapping("/{id}")
     public ResponseEntity<Restaurant> findById(@PathVariable Long id) {
--- src/main/java/com/newfood/delivery/infra/RestaurantRepositoryImpl.java
+++ src/main/java/com/newfood/delivery/infra/RestaurantRepositoryImpl.java
@@ -9,6 +9,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -23,8 +25,15 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQuerys {
         CriteriaBuilder builder = manager.getCriteriaBuilder();
 
         CriteriaQuery<Restaurant> criteriaQuery = builder.createQuery(Restaurant.class);
-        criteriaQuery.from(Restaurant.class); // == "from Restaurant"
-        return manager.createQuery(criteriaQuery).getResultList();
-//        return manager.createQuery("from Restaurant", Restaurant.class).getResultList();
+        Root<Restaurant> root = criteriaQuery.from(Restaurant.class); // == "from Restaurant"
+
+        Predicate namePredicate = builder.like(root.get("name"), "%" + name +"%");
+        Predicate shippingPredicate = builder.greaterThanOrEqualTo(root.get("shipping"), shippingBegin);
+        Predicate shippingFinalPredicate = builder.lessThanOrEqualTo(root.get("shipping"), shippingFinal);
+
+        criteriaQuery.where(namePredicate, shippingPredicate, shippingFinalPredicate);
+
+        TypedQuery<Restaurant> query = manager.createQuery(criteriaQuery);
+        return query.getResultList();
     }
 }
